85142ea24391e9d6e3a2bbd658c5ecf7dc267b81
qianran <qianran@beansmile.com>
2024-02-23 18:28:52 +0800

    feat: @antfu/eslint-config

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const test = ref('Hello World');
 </script>
 
 <template>
   <main class="text-blue-800">
-    home
+    {{ test }}
   </main>
 </template>
